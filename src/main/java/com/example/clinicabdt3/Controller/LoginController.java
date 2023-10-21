123ed7b31b71da89215b620e23afa469d152b30c
Joaquim Emanuel <joaquimemanu13@gmail.com>
2023-10-21 16:09:20 -0300

    Adicionada funcionalidade para trocar a tela quando clicar no botão

--- src/main/java/com/example/clinicabdt3/Controller/LoginController.java
+++ src/main/java/com/example/clinicabdt3/Controller/LoginController.java
@@ -1,13 +1,23 @@
 package com.example.clinicabdt3.Controller;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+//import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+
 import java.io.IOException;
 
 
 public class LoginController {
+    private Parent root;
+    private Scene scene;
+    private Stage stage;
     @FXML
     private Button loginButton;
 
@@ -28,11 +38,25 @@ public class LoginController {
     @FXML
     void login(ActionEvent event) throws IOException {
 
+        //Implementar Banco de Dados
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("paciente_menu.fxml"));
+        root = loader.load();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
     }
 
     @FXML
     void switchToSignUp(ActionEvent event) throws IOException {
-
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("registro.fxml"));
+        root = loader.load();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
     }
-
 }
