520d1aa34b8c6d60572be2dc0da6a321ce3263f9
saleteyara <113694674+saleteyara@users.noreply.github.com>
2023-10-21 15:30:39 -0300

    atualização
    login

--- src/main/java/com/example/clinicabdt3/Controller/LoginController.java
+++ src/main/java/com/example/clinicabdt3/Controller/LoginController.java
@@ -1,5 +1,38 @@
 package com.example.clinicabdt3.Controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+import java.io.IOException;
+
 
 public class LoginController {
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private PasswordField senhaTextField;
+
+    @FXML
+    private TextField CPFTextField;
+
+    @FXML
+    private Button cadastrarButton;
+
+    @FXML
+    void cadastrar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void login(ActionEvent event) throws IOException {
+
+    }
+
+    @FXML
+    void switchToSignUp(ActionEvent event) throws IOException {
+
+    }
 
 }
