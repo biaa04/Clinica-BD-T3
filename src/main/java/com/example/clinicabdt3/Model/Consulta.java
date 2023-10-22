3c41cba333a644c53d6b9f57c79e915b04956941
saleteyara <113694674+saleteyara@users.noreply.github.com>
2023-10-22 12:47:28 -0300

    Update1

--- src/main/java/com/example/clinicabdt3/Model/Consulta.java
+++ src/main/java/com/example/clinicabdt3/Model/Consulta.java
@@ -1,8 +1,13 @@
 package com.example.clinicabdt3.Model;
+import java.util.Date;
 
 public class Consulta {
 
     private String idConsulta;
+    private String paciente;
+
+    private Date date;
+    private String horario;
 
 
     public String getIdConsulta() {
@@ -12,4 +17,28 @@ public class Consulta {
     public void setIdConsulta(String idConsulta) {
         this.idConsulta = idConsulta;
     }
+
+    public String getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(String paciente) {
+        this.paciente = paciente;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
 }
