ea25b40535b786bfd5cbdf964910f04018ff14a2
Joaquim Emanuel <joaquim.emanuel@aluno.uece.br>
2023-10-28 20:54:53 -0300

    Adicionado o controller do menu do adm

---
+++ src/main/java/com/example/clinicabdt3/Controller/MenuAdmController.java
@@ -0,0 +1,17 @@
+package com.example.clinicabdt3.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import org.w3c.dom.Text;
+
+public class MenuAdmController {
+    @FXML
+    TextField nomeTextField;
+
+    @FXML
+    TextField cpfTextField;
+
+    @FXML
+    TextField idadeTextField;
+
+}
