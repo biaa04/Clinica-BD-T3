06ec6757a98b97235167dbbadc9183550d063b5f
Joaquim Emanuel <joaquim.emanuel@aluno.uece.br>
2023-10-28 13:55:50 -0300

    add a função de adicionar, editar e excluir paciente

--- src/main/java/com/example/clinicabdt3/Controller/RegistroController.java
+++ src/main/java/com/example/clinicabdt3/Controller/RegistroController.java
@@ -1,6 +1,7 @@
 package com.example.clinicabdt3.Controller;
 
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
