a07440e7fad8ac6cbc58caf61833eebfc706dc34
Natália Ruth <116367285+nataliaruth04@users.noreply.github.com>
2023-11-05 15:05:25 -0300

    Update App.java

--- src/main/java/com/example/clinicabdt3/App.java
+++ src/main/java/com/example/clinicabdt3/App.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("admPacientes.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Consultas");
         stage.setScene(scene);
