df53707e743edab5f8d767551823599be532a2e5
Natália Ruth <116367285+nataliaruth04@users.noreply.github.com>
2023-11-16 11:30:59 -0300

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,11 +7,19 @@ module com.example.clinicabdt3 {
     opens com.example.clinicabdt3 to javafx.fxml;
     exports com.example.clinicabdt3;
     exports com.example.clinicabdt3.Controller;
-    opens com.example.clinicabdt3.Controller to javafx.fxml;
-    exports com.example.clinicabdt3.Model.Database;
-    opens com.example.clinicabdt3.Model.Database to javafx.fxml;
     exports com.example.clinicabdt3.Model.Domain;
-    opens com.example.clinicabdt3.Model.Domain to javafx.fxml;
+    exports com.example.clinicabdt3.Model.Database;
+
+    opens com.example.clinicabdt3.Controller to javafx.fxml;
+    //exports com.example.clinicabdt3.Model.DAO;
+
+//opens com.example.clinicabdt3 to javafx.fxml;
+ //   exports com.example.clinicabdt3.Model.Database;
+
+    //opens com.example.clinicabdt3.Model.Database to javafx.fxml;
+    //exports com.example.clinicabdt3.Model.Domain;
+
+    //opens com.example.clinicabdt3.Model.Domain to javafx.fxml;
 
 
 }
