ef73ff5f238d07adf12bca7e772babbe0ef90841
Joaquim Emanuel <joaquim.emanuel@aluno.uece.br>
2023-12-02 16:39:11 -0300

    criado o logout do menu do medico

--- src/main/java/com/example/clinicabdt3/Controller/medico_menuController.java
+++ src/main/java/com/example/clinicabdt3/Controller/medico_menuController.java
@@ -1,11 +1,17 @@
 package com.example.clinicabdt3.Controller;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class medico_menuController {
 
@@ -25,11 +31,28 @@ public class medico_menuController {
     private Button logoutbutton;
 
     @FXML
+    private Button editButton;
+
+    @FXML
     private Text nome;
 
     @FXML
-    void logout(ActionEvent event) {
+    void logout(ActionEvent event) throws IOException {
+        System.out.println("oi");
+        Parent root = FXMLLoader.load(LoginController.class.getResource("/com/example/clinicabdt3/login.fxml"));
+        System.out.println("oi");
+        String css = PagePacienteController.class.getResource("/com/example/clinicabdt3/style2.css").toExternalForm();
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(css);
 
+        Stage stage = (Stage) ((javafx.scene.Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.show();
     }
+
+    /*void editarDados(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load();
+    }*/
 
 }
